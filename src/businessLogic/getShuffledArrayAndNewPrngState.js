8c4ca9971e6bde87b75c355719b84a3028988053
Kyle Lin <kylejlin.us@gmail.com>
2018-11-12 15:28:45 -0800

    Remove unnecessary prng console.log statements

--- src/businessLogic/getShuffledArrayAndNewPrngState.js
+++ src/businessLogic/getShuffledArrayAndNewPrngState.js
@@ -3,14 +3,12 @@ import xorshift from "xorshift";
 const Xorshift = xorshift.constructor;
 
 export default ({ array, prngState }) => {
-  console.log("old", prngState);
   const xorshift = new Xorshift(prngState);
   const shuffledArray = array.slice();
   for (let i = 0; i < 256; i++) {
     shuffledArray.sort(() => xorshift.random() - 0.5);
   }
   const newPrngState = xorshift.randomint().concat(xorshift.randomint());
-  console.log("new", newPrngState);
   return {
     shuffledArray,
     newPrngState,
