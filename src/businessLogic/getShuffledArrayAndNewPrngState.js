dddf582982f2738acf221700c08dc0b94bc5b7be
Kyle Lin <kylejlin.us@gmail.com>
2018-11-24 14:44:35 -0800

    Use an O(n) Fisher-Yates shuffle algorithm
    Previously, the deck was shuffled using `Array.sort` and a PRNG.
    The implementation of `Array.sort` varies from browser to browser.
    This meant that players using different browsers would see different
    outcomes.

--- src/businessLogic/getShuffledArrayAndNewPrngState.js
+++ src/businessLogic/getShuffledArrayAndNewPrngState.js
@@ -5,8 +5,16 @@ const Xorshift = xorshift.constructor;
 export default ({ array, prngState }) => {
   const xorshift = new Xorshift(prngState);
   const shuffledArray = array.slice();
+  const lastIndex = array.length - 1;
   for (let i = 0; i < 256; i++) {
-    shuffledArray.sort(() => xorshift.random() - 0.5);
+    // O(n) shuffle
+    // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+    for (let j = lastIndex; j > 0; j--) {
+      const k = Math.floor(xorshift.random() * (j + 1));
+      const temp = shuffledArray[j];
+      shuffledArray[j] = shuffledArray[k];
+      shuffledArray[k] = temp;
+    }
   }
   const newPrngState = xorshift.randomint().concat(xorshift.randomint());
   return {
